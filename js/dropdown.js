d89366e807c5392a8d648dfc9d4a40cbda17621e
majraoruc <majra.oruc@stu.ibu.edu.ba>
2020-07-06 12:31:20 +0200

    Full page

---
+++ js/dropdown.js
@@ -0,0 +1,12 @@
+document.getElementById("menu-button").onclick =  function() {Izlazi()};
+
+function Izlazi(){
+    function Izlazi() {
+        var x = document.getElementById("menu");
+        if (x.style.display === "none") {
+          x.style.display = "block";
+        } else {
+          x.style.display = "none";
+        }
+      }
+}
---
+++ js/swiper.js
@@ -0,0 +1,90 @@
+var swiper1 = new Swiper('.swiper-container1', {
+
+  slidesPerView: 4,
+  spaceBetween: 2,
+  loop:true,
+pagination: {
+  el: '.swiper-pagination',
+  clickable: true,
+},
+navigation: {
+  nextEl: '.swiper-button-next',
+  prevEl: '.swiper-button-prev',
+},
+breakpoints:{
+
+  1381:{
+      slidesPerView:4,
+      spaceBetween: 10
+  },  
+  1380:{
+      slidesPerView:3,
+      spaceBetween: 30
+  },
+  1000: {
+      slidesPerView:3,
+      spaceBetween: 30
+  },
+
+  768: {
+      slidesPerView:2,
+      spaceBetween: 30
+  },
+
+  576: {
+      slidesPerView:1,
+      spaceBetween: 30
+  },
+
+  320: {
+      slidesPerView:1,
+      spaceBetween: 30
+  }
+ 
+}
+//    breakpoints: {  
+//       1380: {       
+//          slidesPerView: 3,       
+//          spaceBetween: 2     
+//       },
+//       1220: {       
+//         slidesPerView: 3,       
+//         spaceBetween: 2     
+//      } 
+//    } 
+});
+
+var swiper2 = new Swiper('.swiper-container2', {
+  slidesPerView: 3,
+  spaceBetween: 2,
+  clickable:true,
+  loop:true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
+
+var swiper = new Swiper('.swiper-container3', {
+  spaceBetween: 30,
+  centeredSlides: true,
+  loop:true,
+  slidesPerSlide:1,
+  
+  autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+  },
+  pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+  },
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+  }
+});
