39415aae8876833eb251e6fb03aafdec0b43812d
LejlaTasakovic <lejla.tasakovic97@gmail.com>
2020-07-06 19:57:42 +0200

    More improvements

--- js/swiper.js
+++ js/swiper.js
@@ -105,10 +105,10 @@ var swiper = new Swiper('.swiper-container3', {
   loop:true,
   slidesPerSlide:1,
   
-  autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-  },
+  // autoplay: {
+  //     delay: 2500,
+  //     disableOnInteraction: false,
+  // },
   pagination: {
       el: '.swiper-pagination',
       clickable: true,
