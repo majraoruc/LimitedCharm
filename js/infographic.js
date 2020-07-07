3b137cf065f62cf8ee35da3f4c269efcabc05b74
LejlaTasakovic <lejla.tasakovic97@gmail.com>
2020-07-07 13:45:22 +0200

    changes again

--- js/infographic.js
+++ js/infographic.js
@@ -1,13 +1,131 @@
+function ActivePlaceholder() {
 
-var i = 0;
-var txt = 'Tražiš dašak prirode? Na pravom si mjestu.';
-var speed = 50;
 
-function typeWriter() {
-    if (i < txt.length) {
-        document.getElementById("input").placeholder += txt.charAt(i);
-        i++;
-        setTimeout(typeWriter, speed);
+    if (arguments.length === 1) {
+        var element = arguments[0];
+        this.base = null;
+        this.placeholders = null;
+
+        this.fBase = 24;
+        this.fBuild = 12;
+        this.fFull = 24;
+        this.fRemove = 12;
+    }
+    else if (arguments.length === 3) {
+        var element = arguments[0];
+        this.base = arguments[1];
+        this.placeholders = arguments[2];
+
+        this.fBase = 24;
+        this.fBuild = 12;
+        this.fFull = 24;
+        this.fRemove = 12;
+    }
+    else if (arguments.length === 7) {
+        var element = arguments[0];
+        this.base = arguments[1];
+        this.placeholders = arguments[2];
+
+        this.fBase = arguments[3];
+        this.fBuild = arguments[4];
+        this.fFull = arguments[5];
+        this.fRemove = arguments[6];
+    }
+
+
+    var state = 'base';
+    var currentWord = null;
+    var currentWordIndex = 0;
+    var countdown = 0;
+    var animation = null;
+
+
+    var fps = 24;
+
+    this.start = function () {
+        if (this.base == null || this.placeholders == null) {
+            console.log("No base or placeholders. Make sure you know what you're doing.");
+            return;
+        }
+        if (animation) {
+            console.log("Active placeholder is already running.");
+            return;
+        }
+
+        // Closures or something
+        var activePlaceholder = this;
+
+        animation = window.setInterval(function () {
+            switch (state) {
+                case 'base':
+                    if (countdown > 0) {
+                        if (--countdown === 0) {
+                            state = 'build';
+                        }
+                    } else {
+                        currentWord = activePlaceholder.placeholders[currentWordIndex];
+                        element.placeholder = activePlaceholder.base;
+                        countdown = activePlaceholder.fBase;
+                        currentWordIndex = (currentWordIndex + 1) % activePlaceholder.placeholders.length;
+                    }
+                    break;
+
+                case 'build':
+                    if (countdown > 0) {
+                        var percent = (activePlaceholder.fBuild - countdown) / activePlaceholder.fBuild;
+                        var addon = currentWord.substring(0, Math.floor(percent * currentWord.length));
+                        element.placeholder = activePlaceholder.base + addon;
+                        if (--countdown === 0) {
+                            state = 'full';
+                        }
+                    } else {
+                        countdown = activePlaceholder.fBuild;
+                    }
+                    break;
+
+                case 'full':
+                    if (countdown > 0) {
+                        if (--countdown === 0) {
+                            state = 'remove';
+                        }
+                    } else {
+                        element.placeholder = activePlaceholder.base + currentWord;
+                        countdown = activePlaceholder.fFull;
+                    }
+                    break;
+
+                case 'remove':
+                    if (countdown > 0) {
+                        var percent = countdown / activePlaceholder.fRemove;
+                        var addon = currentWord.substring(0, Math.floor(percent * currentWord.length));
+                        element.placeholder = activePlaceholder.base + addon;
+                        if (--countdown === 0) {
+                            state = 'base';
+                        }
+                    } else {
+                        countdown = activePlaceholder.fRemove;
+                    }
+                    break;
+
+                default:
+                    console.log('Something went horribly wrong: ' + activePlaceholder.base);
+                    break;
+            }
+        }, 1000 / fps);
+    }
+
+	/*
+	Stops animation, sets the <input>'s placeholder to base + full placeholder regardless of state.
+	*/
+    this.stop = function () {
+        window.clearInterval(animation);
+        animation = false;
+        element.placeholder = this.base + this.placeholders[currentWordIndex];
+        state = 'full';
+        countdown = 0;
     }
 }
-typeWriter();
+
+var input2 = document.getElementById('input');
+var activePlaceholder = new ActivePlaceholder(input, '', ['Tražiš dašak prirode?', 'Na pravom si mjestu!']);
+activePlaceholder.start();
--- js/swiper.js
+++ js/swiper.js
@@ -2,63 +2,7 @@ var swiper1 = new Swiper('.swiper-container1', {
 
   slidesPerView: 4,
   spaceBetween: 2,
-  loop:true,
-pagination: {
-  el: '.swiper-pagination',
-  clickable: true,
-},
-navigation: {
-  nextEl: '.swiper-button-next',
-  prevEl: '.swiper-button-prev',
-},
-breakpoints:{
-
-  1381:{
-      slidesPerView:4,
-      spaceBetween: 10
-  },  
-  1380:{
-      slidesPerView:3,
-      spaceBetween: 30
-  },
-  1000: {
-      slidesPerView:3,
-      spaceBetween: 30
-  },
-
-  768: {
-      slidesPerView:2,
-      spaceBetween: 30
-  },
-
-  576: {
-      slidesPerView:1,
-      spaceBetween: 30
-  },
-
-  320: {
-      slidesPerView:1,
-      spaceBetween: 30
-  }
- 
-}
-//    breakpoints: {  
-//       1380: {       
-//          slidesPerView: 3,       
-//          spaceBetween: 2     
-//       },
-//       1220: {       
-//         slidesPerView: 3,       
-//         spaceBetween: 2     
-//      } 
-//    } 
-});
-
-var swiper2 = new Swiper('.swiper-container2', {
-  slidesPerView: 3,
-  spaceBetween: 2,
-  clickable:true,
-  loop:true,
+  loop: true,
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
@@ -67,54 +11,114 @@ var swiper2 = new Swiper('.swiper-container2', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-  breakpoints:{
-  
-    1381:{
-        slidesPerView:3,
-        spaceBetween: 10
-    },  
-    1220:{
-        slidesPerView:2,
-        spaceBetween: 30
+  breakpoints: {
+
+    1381: {
+      slidesPerView: 4,
+      spaceBetween: 10
+    },
+    1380: {
+      slidesPerView: 3,
+      spaceBetween: 30
     },
     1000: {
-      slidesPerView:1,
+      slidesPerView: 3,
       spaceBetween: 30
-  },
+    },
 
-  768: {
-      slidesPerView:1,
+    768: {
+      slidesPerView: 2,
       spaceBetween: 30
-  },
+    },
 
-  576: {
-      slidesPerView:1,
+    576: {
+      slidesPerView: 1,
       spaceBetween: 30
-  },
+    },
 
-  320: {
-      slidesPerView:1,
+    320: {
+      slidesPerView: 1,
       spaceBetween: 30
+    }
+
   }
+  //    breakpoints: {  
+  //       1380: {       
+  //          slidesPerView: 3,       
+  //          spaceBetween: 2     
+  //       },
+  //       1220: {       
+  //         slidesPerView: 3,       
+  //         spaceBetween: 2     
+  //      } 
+  //    } 
+});
+
+var swiper2 = new Swiper('.swiper-container2', {
+  slidesPerView: 3,
+  spaceBetween: 2,
+  clickable: true,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+
+    1381: {
+      slidesPerView: 3,
+      spaceBetween: 10
+    },
+    1220: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    1100:{
+      slidesPerView: 2,
+      spaceBetween: 10
+    },
+    1000: {
+      slidesPerView: 1,
+      spaceBetween: 30
+    },
+
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 30
+    },
+
+    576: {
+      slidesPerView: 1,
+      spaceBetween: 30
+    },
+
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 30
+    }
   }
 });
 
 var swiper = new Swiper('.swiper-container3', {
   spaceBetween: 30,
   centeredSlides: true,
-  loop:true,
-  slidesPerSlide:1,
-  
-  // autoplay: {
-  //     delay: 2500,
-  //     disableOnInteraction: false,
-  // },
+  loop: true,
+  slidesPerSlide: 1,
+
+  autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+  },
   pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
+    el: '.swiper-pagination',
+    clickable: true,
   },
   navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   }
 });
