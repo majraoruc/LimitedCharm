c6863a4665458594fa80a270fc42271e012a8322
LejlaTasakovic <lejla.tasakovic97@gmail.com>
2020-07-07 17:02:37 +0200

    ne znam

--- js/dropdown.js
+++ js/dropdown.js
@@ -10,3 +10,13 @@ function Izlazi(){
         }
       }
 }
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+    x.style.backgroundColor = "#377d3c";
+  }
+}
